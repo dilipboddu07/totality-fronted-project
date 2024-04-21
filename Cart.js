a1aee430df366ccf4e7dbe15e3c258fd8f81d976
dilipboddu07 <130659804+dilipboddu07@users.noreply.github.com>
2024-04-21 20:56:03 +0530

    Update Cart.js

--- Cart.js
+++ Cart.js
@@ -6,19 +6,29 @@ const Cart = () => (
   <CartContext.Consumer>
     {(value) => {
       const { CartCont } = value;
+      const length = CartCont.length;
       return (
         <div>
-          <Header />
-          <div className="CartContainer">
-            {CartCont.map((each) => (
-              <CartEditor each={each} key={each.key} />
-            ))}
-          </div>
+          {length === 0 ? (
+            <div className="CartContainer">
+              <Header />
+              <h1>Cart Empty</h1>
+            </div>
+          ) : (
+            <div>
+              <div>
+                <Header />
+                <div className="CartContainer">
+                  {CartCont.map((each) => (
+                    <CartEditor each={each} key={each.key} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       );
     }}
   </CartContext.Consumer>
 );
 export default Cart;
-
-
